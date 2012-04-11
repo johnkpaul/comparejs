2870b4b208fe5726d542d66acb32c940450a7a2b
John Paul <john@johnkpaul.com>
2012-04-11 14:58:02 -0400

    add example mocha test

---
+++ test/app-test.js
@@ -0,0 +1,10 @@
+var should = require('should');
+
+describe('Person', function() {
+        it('should be able to say hello', function() {
+                var message = "test";
+                message.should.equal("test");
+//                throw new Error();
+        });
+});
+
